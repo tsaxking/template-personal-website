502c841a08c8e59402102b32252b26557690bf24
tsaxking <taylorking206@u.boisestate.edu>
2023-11-14 06:53:15 -0700

    add entry points

--- client/entries/test/stream.ts
+++
@@ -1,8 +0,0 @@
-import '../../utilities/imports';
-
-import { ServerRequest } from '../../utilities/requests';
-
-const s = ServerRequest.retrieveStream('/test-stream');
-
-s.on('chunk', console.log);
-s.on('complete', console.log);
